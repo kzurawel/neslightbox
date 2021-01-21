fb70c4531840961ba5d539efb98aedcbad4d2fc9
Kevin Zurawel <kzurawel@gmail.com>
2021-01-21 16:28:36 -0600

    Set bgcolor; fix MacOS app behavior

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
 const RatioWindow = require('./utils/ratiowindow.js');
 const path = require('path');
 
@@ -32,19 +32,7 @@ app.on('ready', createWindow);
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (RatioWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+app.on('window-all-closed', app.quit);
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
